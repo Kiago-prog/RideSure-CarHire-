ca9ccd39549da5a3b9c815e547cecd08af2654b7
Alicia-Natasha <alicia.natasha@student.moringaschool.com>
2025-04-27 00:08:49 +0300

    backend bit

--- ridesure/src/App.js
+++ ridesure/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from './Components/navbar';
+import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import SearchForm from './Components/SearchForm';
 import CarList from './components/CarList';
@@ -7,11 +7,12 @@ import CarList from './components/CarList';
 
 function App() {
 return(
-  <div>
+  <div className="App">
     <Navbar/>
-    <Home/>
+    <h1>RideSure Car Hire</h1>
     <SearchForm />
     <CarList />
+    <Home/>
   </div>
 );
 }
--- ridesure/src/Components/CarList.js
+++ ridesure/src/Components/CarList.js
@@ -1,44 +1,24 @@
-import React from 'react';
+import React from "react";
 
-const dummyCars = [
-    {
-      id: 1,
-      name: 'Toyota Corolla',
-      image: 'https://source.unsplash.com/400x200/?toyota,car',
-      pricePerDay: 45,
-      transmission: 'Automatic',
-      fuel: 'Petrol',
-    },
-    {
-      id: 2,
-      name: 'Honda Civic',
-      image: 'https://source.unsplash.com/400x200/?honda,car',
-      pricePerDay: 55,
-      transmission: 'Manual',
-      fuel: 'Diesel',
-    },
-    {
-      id: 3,
-      name: 'Tesla Model 3',
-      image: 'https://source.unsplash.com/400x200/?tesla,car',
-      pricePerDay: 90,
-      transmission: 'Automatic',
-      fuel: 'Electric',
-    },
-  ];
-  
-  const CarList = () => {
-    return (
-      <section className="car-list">
-        <h2>Available Cars</h2>
-        <div className="car-grid">
-          {dummyCars.map((car) => (
-            <CarCard key={car.id} car={car} />
-          ))}
-        </div>
-      </section>
-    );
-  };
-  
-  export default CarList;
-  
+const cars = [
+  { id: 1, brand: "Toyota", model: "Corolla", year: 2020, type: "Sedan" },
+  { id: 2, brand: "Ford", model: "Ranger", year: 2022, type: "Truck" },
+  { id: 3, brand: "Honda", model: "Civic", year: 2021, type: "Sedan" },
+];
+
+function CarList() {
+  return (
+    <div>
+      <h2>Available Cars for Hire</h2>
+      <ul>
+        {cars.map((car) => (
+          <li key={car.id}>
+            {car.brand} {car.model} ({car.year}) - {car.type}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default CarList;
