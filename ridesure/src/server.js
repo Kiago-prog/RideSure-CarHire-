9f948f5ca7bd6becc941aa3302053976108e300a
Alicia-Natasha <alicia.natasha@student.moringaschool.com>
2025-04-27 00:43:25 +0300

    resolved conflicts

--- ridesure/src/server.js
+++ ridesure/src/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 4000;
+const PORT = 3000;
 
 const cars =  [
     {id: 1, brand: "Toyota", model: "Corolla", year: 2020, type:"Sedan"},
